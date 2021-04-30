26731cd89b1fb2dfa3dcdc712b5dc7252e4ece4d
ume388 <turi388ouji@gmail.com>
2021-04-30 20:50:23 +0000

    [add]ログイン機能周辺

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
 
   scope module: 'user' do
     root to: 'homes#top'
