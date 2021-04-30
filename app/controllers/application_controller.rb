d8de2ba9fe4e31c20017fea5c89d21d76d499c6b
ume388 <turi388ouji@gmail.com>
2021-04-30 20:50:23 +0000

    [add]ログイン機能周辺

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parametes, if: :devise_controller?
+  
+  protected
+  
+  def configure_permitted_parametes
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+  end
+  
 end
