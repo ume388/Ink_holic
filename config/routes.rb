9eb2f127ba4d1dab7fb68df47852405a4056c456
ume388 <turi388ouji@gmail.com>
2021-04-30 20:13:48 +0000

    [add]userテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
 
   scope module: 'user' do
     root to: 'homes#top'
