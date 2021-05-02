6f81bd035bef0d145ef1bbf019e6069de105079a
ume388 <turi388ouji@gmail.com>
2021-05-02 03:32:22 +0000

    [add]いいね機能

--- app/models/ink.rb
+++ app/models/ink.rb
@@ -1,8 +1,13 @@
 class Ink < ApplicationRecord
   
   belongs_to :user
+  has_many :favorites, dependent: :destroy
   attachment :ink_image
   
   enum color: {"黒・灰色系": 0, "青・水色系": 1, "緑系": 2, "黄・オレンジ系": 3, "赤・ピンク系": 4, "紫系": 5, "茶色系": 6, "その他": 7 }
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :inks, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   attachment :user_image
   
 end
