3c68732a9b6de46e89706a16b357ba69f7817bc4
ume388 <turi388ouji@gmail.com>
2021-05-04 20:58:59 +0000

    [add]管理者モデル

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
@@ -15,5 +21,5 @@ Rails.application.routes.draw do
     get 'unsubscribe' => 'users#unsubscribe'
     post 'withdraw' => 'users#withdraw'
   end
-  
+
 end
