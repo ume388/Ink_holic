0e88a73a5eeeb324370f8c0f705b83309514e705
ume388 <turi388ouji@gmail.com>
2021-05-04 05:44:22 +0000

    [add]マイページにユーザー編集画面へのリンク追加

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,6 +1,7 @@
 class User::UsersController < ApplicationController
   
   def show
+    @user = current_user
     @inks = current_user.inks
     @favorite_inks = current_user.favorites
 
@@ -12,6 +13,25 @@ class User::UsersController < ApplicationController
     gon.purple = current_user.available_inks(5)
     gon.brown = current_user.available_inks(6)
     gon.other = current_user.available_inks(7)
+  end
+  
+  def edit
+    @user = current_user
+  end
+  
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(user)
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:user_name, :email, :user_image, :is_deleted)
   end
   
 end
