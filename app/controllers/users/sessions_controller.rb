76e929db754f2ec16a6a97e7332247629a85eb11
ume388 <turi388ouji@gmail.com>
2021-05-04 08:06:38 +0000

    [update]退会ユーザーのログイン不可機能

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :rejected_user, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -18,7 +18,16 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  
+  def rejected_user
+    @user = User.find_by(email: params[:user][:email].downcase)
+    if @user
+      if (@user.valid_password?(params[:user][:password]) && (@user.is_deleted == true))
+        redirect_to new_user_session_path, notice: "すでに退会しています。"
+      end
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
