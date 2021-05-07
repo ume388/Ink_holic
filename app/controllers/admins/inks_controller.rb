25fc0ec0c983ac4fdb56f37234554890e54ef479
ume388 <turi388ouji@gmail.com>
2021-05-07 20:59:06 +0000

    [add]管理者用インク編集画面

--- app/controllers/admins/inks_controller.rb
+++ app/controllers/admins/inks_controller.rb
@@ -7,5 +7,33 @@ class Admins::InksController < ApplicationController
   def show
     @ink = Ink.find(params[:id])
   end
+  
+  def edit
+    @ink = Ink.find(params[:id])
+  end
+  
+  def update
+    @ink = Ink.find(params[:id])
+    if @ink.update(ink_params)
+      redirect_to admins_inks_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @ink = Ink.find(params[:id])
+    if @ink.destroy
+      redirect_to admins_inks_path
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def ink_params
+     params.require(:ink).permit(:ink_name, :ink_image, :ink_introduction, :price, :milliliter, :evaluation, :color)
+  end
 
 end
