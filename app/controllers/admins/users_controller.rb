5450a5b1a38fd14d4eab9d71213136ff8c8c553e
ume388 <turi388ouji@gmail.com>
2021-05-09 01:11:21 +0000

    [add]管理者用ユーザー編集画面

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -9,4 +9,23 @@ class Admins::UsersController < ApplicationController
     @inks = @user.inks
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admins_user_path(@user)
+    else
+      edit_admins_user_path(@user)
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:user_name, :email, :is_deleted, :user_image)
+  end
+
 end
