82612f5effa7e9664546a437eb7755fb01764586
ume388 <turi388ouji@gmail.com>
2021-05-09 20:13:58 +0000

    [add]ユーザーからの通報機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :reports
   has_many :events
   has_many :inks, dependent: :destroy
   has_many :favorites, dependent: :destroy
