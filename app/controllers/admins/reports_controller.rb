d9da33d597a79e5a73055e688a9be80e0cb5936b
ume388 <turi388ouji@gmail.com>
2021-05-10 19:51:26 +0000

    [add]管理者用通報編集画面

--- app/controllers/admins/reports_controller.rb
+++ app/controllers/admins/reports_controller.rb
@@ -8,4 +8,23 @@ class Admins::ReportsController < ApplicationController
     @report = Report.find(params[:id])
   end
   
+  def edit
+    @report = Report.find(params[:id])
+  end
+  
+  def update
+    @report = Report.find(params[:id])
+    if @report.update(report_params)
+      redirect_to admins_reports_path
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def report_params
+    params.require(:report).permit(:report_title, :sentence, :user_id, :is_comfortable)
+  end
+  
 end
