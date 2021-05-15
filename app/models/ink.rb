37356ba36472ec894964cff8bf17907ddca4d0b2
ume388 <turi388ouji@gmail.com>
2021-05-15 22:08:40 +0000

    [update]バリデーションの追加

--- app/models/ink.rb
+++ app/models/ink.rb
@@ -6,6 +6,9 @@ class Ink < ApplicationRecord
   
   enum color: {"黒・灰色系": 0, "青・水色系": 1, "緑系": 2, "黄・オレンジ系": 3, "赤・ピンク系": 4, "紫系": 5, "茶色系": 6, "その他": 7 }
   
+  validates :ink_name, presence: true
+  validates :ink_image, presence: true
+  
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
