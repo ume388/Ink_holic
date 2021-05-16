f8d8026763d9c211d92cc1f00f3dae63e138da5f
ume388 <turi388ouji@gmail.com>
2021-05-16 04:31:15 +0000

    [update]管理者画面へページネーションの追加

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -2,7 +2,7 @@ class Admins::UsersController < ApplicationController
   before_action :authenticate_admin!
   
   def index
-    @users = User.all
+    @users = User.page(params[:page]).reverse_order
   end
   
   def show
